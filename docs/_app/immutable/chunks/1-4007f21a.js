70ca524b046161857ac28b54479d2734f89f3d88
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-10-01 14:56:15 +0200

    feat: splash img

---
+++ docs/_app/immutable/chunks/1-4007f21a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-3d50257c.js";export{t as component};
--- docs/_app/immutable/chunks/1-768f4b0b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-8d9fc016.js";export{t as component};
