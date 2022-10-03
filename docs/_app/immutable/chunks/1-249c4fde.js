a94b1e9788be454046c4d34ebd3ba1c669b0bde9
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-10-03 21:27:53 +0200

    fix: barnstorm banner path

---
+++ docs/_app/immutable/chunks/1-249c4fde.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-08b44790.js";export{t as component};
--- docs/_app/immutable/chunks/1-5480453f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-452e6215.js";export{t as component};
